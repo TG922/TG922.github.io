9bcdf8c2cf3b9e9787521c092478330890194f79
Tarun Gaur <70098839+TG922@users.noreply.github.com>
2022-04-03 00:44:47 +0530

    Add files via upload

---
+++ js/carousel-owl-carousel.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+$(".carousel-owl-carousel").owlCarousel({
+
+autoPlay: 3000,
+items : 4,
+itemsDesktop : [1199,3],
+itemsDesktopSmall : [979,3],
+center: true,
+nav:true,
+loop:true,
+responsive: {
+600: {
+items: 4
+}
+}
+
+
+
+
+
+
+});
+
+});
